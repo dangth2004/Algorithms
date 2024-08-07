6a2f20c9588ffbedab543137340bef33e8226517
dangth2004 <trinhhaidang2004@gmail.com>
2024-08-07 22:50:54 +0700

    Update MyList.java

--- src/dsa/list/MyList.java
+++ src/dsa/list/MyList.java
@@ -1,5 +1,10 @@
 package dsa.list;
 
+/**
+ * Interface for a generic list.
+ *
+ * @param <E> the type of elements in this list
+ */
 public interface MyList<E> {
     /**
      * Returns the number of elements in this list.
@@ -16,31 +21,67 @@ public interface MyList<E> {
     boolean isEmpty();
 
     /**
-     * Adds a new node with the specified value at the end of the list.
+     * Adds a new element with the specified value at the end of the list.
      *
      * @param key the value to be added to the list
      */
     void add(E key);
 
     /**
-     * Inserts a new node with the specified value at the given index.
+     * Adds a new element with the specified value at the given index.
      * Index starts from 0.
      *
-     * @param key   the value to be inserted
-     * @param index the index at which the new node is to be inserted
+     * @param key   the value to be added
+     * @param index the index at which the new element is to be added
      * @throws IndexOutOfBoundsException if the index is out of range
      */
-    void insert(E key, int index);
+    void add(E key, int index);
 
     /**
-     * Returns the value of the node at the specified index.
+     * Adds a new element with the specified value at the beginning of the list.
+     *
+     * @param key the value to be added
+     */
+    void addFirst(E key);
+
+    /**
+     * Adds a new element with the specified value at the end of the list.
+     *
+     * @param key the value to be added
+     */
+    void addLast(E key);
+
+    /**
+     * Returns true if this list contains the specified element.
+     *
+     * @param key the value to search for
+     * @return true if the list contains the specified element, false otherwise
+     */
+    boolean contains(E key);
+
+    /**
+     * Returns the value of the element at the specified index.
      * Index starts from 0.
      *
-     * @param index the index of the node whose value is to be returned
-     * @return the value of the node at the specified index
+     * @param index the index of the element whose value is to be returned
+     * @return the value of the element at the specified index
      * @throws IndexOutOfBoundsException if the index is out of range
      */
     E get(int index);
+
+    /**
+     * Returns the value of the first element in the list.
+     *
+     * @return the value of the first element in the list
+     */
+    E getFirst();
+
+    /**
+     * Returns the value of the last element in the list.
+     *
+     * @return the value of the last element in the list
+     */
+    E getLast();
 
     /**
      * Replaces the element at the specified position in this list with the specified element.
@@ -52,11 +93,40 @@ public interface MyList<E> {
     void set(E key, int index);
 
     /**
-     * Removes the node at the specified index.
+     * Removes the element at the specified index.
      * Index starts from 0.
      *
-     * @param index the index of the node to be removed
+     * @param index the index of the element to be removed
      * @throws IndexOutOfBoundsException if the index is out of range
      */
     void remove(int index);
+
+    /**
+     * Removes and returns the first element of the list.
+     *
+     * @return the first element of the list
+     */
+    E removeFirst();
+
+    /**
+     * Removes and returns the last element of the list.
+     *
+     * @return the last element of the list
+     */
+    E removeLast();
+
+    /**
+     * Removes all elements from the list.
+     */
+    void clear();
+
+    /**
+     * Returns the index of the first occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element.
+     *
+     * @param key the element to search for
+     * @return the index of the first occurrence of the specified element in this list,
+     *         or -1 if this list does not contain the element
+     */
+    int indexOf(E key);
 }
